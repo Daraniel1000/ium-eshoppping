965f9297ec9362d79b48195a10a1836ef45f79f2
Sebastian Pietras <seba00767@gmail.com>
2020-06-18 17:51:06 +0200

    Implemented sample predictions

--- server/server/__main__.py
+++ server/server/__main__.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from flask import Flask, jsonify, request, abort, render_template
+from sklearn.dummy import DummyClassifier
 
 from server import loaders
+from server import predictions
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
@@ -15,14 +17,14 @@ def index():
 @app.route('/users', methods=['GET'])
 def users():
     return jsonify({
-        "users": users_df.to_dict(orient="records")
+        "users": users_api_df.to_dict(orient="records")
     })
 
 
 @app.route('/categories', methods=['GET'])
 def categories():
     return jsonify({
-        'categories': categories_df.to_dict(orient="records")
+        'categories': categories_api_df.to_dict(orient="records")
     })
 
 
@@ -32,7 +34,7 @@ def products():
     if not category:
         abort(400, "Parameter \"category\" not passed")
     return jsonify({
-        'products': products_df[products_df["category"] == category].to_dict(orient="records")
+        'products': products_api_df[products_api_df["category"] == category].to_dict(orient="records")
     })
 
 
@@ -44,9 +46,12 @@ def predict():
         abort(400, "Parameter \"user\" not passed")
     if not product_id:
         abort(400, "Parameter \"product\" not passed")
-    return jsonify({
-        'prediction': 0.5
-    })
+    try:
+        return jsonify({
+            'predicted_discount': predictions.predict_discount(user_id, product_id, products_df, sessions_df, model)
+        })
+    except IndexError as e:
+        abort(400, str(e))
 
 
 def load_dataset(base_path, file):
@@ -61,10 +66,15 @@ if __name__ == '__main__':
     data_base_path = "data/"
 
     try:
-        users_df = loaders.get_api_users(data_base_path)
-        categories_df = loaders.get_api_categories(data_base_path)
-        products_df = loaders.get_api_products(data_base_path)
-        aggregated_df = loaders.get_aggregated(data_base_path)
+        users_df = loaders.load_users(data_base_path)
+        products_df = loaders.load_products(data_base_path)
+        sessions_df = loaders.load_sessions(data_base_path, products_df)
+
+        users_api_df = users_df[["user_id", "name"]]
+        categories_api_df = pd.DataFrame(products_df["category"].unique(), columns=["name"])
+        products_api_df = products_df
+
+        model = DummyClassifier(strategy="uniform").fit([list(range(15)), list(range(15))], [0, 1])
     except IOError as e:
         print("Can't load server. Error occurred: ", e)
         exit(1)
